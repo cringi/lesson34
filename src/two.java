d7c617025cc0fa82a8cd675b6ce57e9ee78863cf
Brandon Berney <brandonberney@protonmail.com>
2016-02-07 17:56:13 -0500

    do some beautiful coding for part two

--- src/two.java
+++ src/two.java
@@ -19,18 +19,32 @@
 import java.util.Scanner;
 
 class two {
+    public static double average(int[] list) {
+        int sum = 0, average;
 
-    public static double average(int [] a)
-    {
-        //Write your code here
-        //Change the return value as you see fit
-        return 0;
+        for (int value:list) {
+            sum += value;
+        }
+
+        average = sum/list.length;
+        return average;
     }
 
-    public static void main(String[] args)
-    {
-     /*
-      * Test your method here
-      */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter an list of number, separate with a space: ");
+        String data = input.nextLine();
+
+        // Put what we currently have into an array
+        String[] _list = data.split(" ");
+        int[] list = new int[_list.length];
+
+        // Convert this to an integer array
+        for(int x=0;x<list.length;x++) {
+            list[x] = Integer.parseInt(_list[x]);
+        }
+
+        System.out.println("Average: " + average(list));
     }
 }
